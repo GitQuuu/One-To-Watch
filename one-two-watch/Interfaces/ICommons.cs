57de31d6b3a03cbeaa24252c483b45b1c841e1ae
Qu <Quan0027@elec.techcollege.dk>
2020-06-18 09:11:10 +0200

    add a ICommons Interface to Interfaces - https://www.geeksforgeeks.org/c-sharp-interface/
    https://www.geeksforgeeks.org/c-sharp-interface/

---
+++ one-two-watch/Interfaces/ICommons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace one_two_watch.Interfaces
+{
+    interface ICommons
+    {
+        void Start();
+        void Stop();
+
+    }
+}
