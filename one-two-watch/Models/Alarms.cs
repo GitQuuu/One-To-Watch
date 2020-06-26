9e808ccd1e9c029d76c417fe0f7b5e16622e7719
Qu <Quan0027@elec.techcollege.dk>
2020-06-26 10:47:30 +0200

    Gui to Alarm done - now onto the logic

--- one-two-watch/Models/Alarms.cs
+++ one-two-watch/Models/Alarms.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace one_two_watch.Models
 {
-    class Alarms
+    class Alarms: Stopwatch
     {
+        ObservableCollection<DateTime> alarmsCollection = new ObservableCollection<DateTime>();
+        public DateTime AlertTime { get; set; }
     }
 }
